70ccf6e0153d981afb9097d5b6cc146042e4ef92
yungdeni <asertyx@gmail.com>
2020-10-30 01:23:49 +0100

    klar!

--- Personnummer/Program.cs
+++ Personnummer/Program.cs
@@ -13,24 +13,24 @@ namespace Personnummer
         {
             //Init an array where index is the month(-1) and the value is the length in days
             int[] monthLength = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
             Console.WriteLine("Skriv in ditt personnummer");
             string userInput = Console.ReadLine();
-
-            
             if (userInput.Length == 11)
             {
                 if (int.TryParse(userInput.Substring(0, 6), out int yearCheck))
                 {
-                    
-                    //People with + are older than 100
                     if (userInput.Substring(6, 1) == "-" && !CompareToCurrentDate(userInput.Substring(0, 6)))
                     {
                         userInput = userInput.Insert(0, "19");
                     }
-                    else if (userInput.Substring(6, 1) == "+")
+                    else if (userInput.Substring(6, 1) == "-")
                     {
-                        if (CompareToCurrentDate(userInput.Substring(0, 6)))
+                        userInput = userInput.Insert(0, "20");
+                    }
+                    //People with + are older than 100
+                    if (userInput.Substring(6, 1) == "+")
+                    {
+                        if (CheckIfOverHundredYears(userInput.Substring(0, 6)) && !CompareToCurrentDate(userInput.Substring(0, 6)))
                         {
                             userInput = userInput.Insert(0, "18");
                         }
@@ -38,26 +38,11 @@ namespace Personnummer
                         {
                             userInput = userInput.Insert(0, "19");
                         }
-
                     }
-                    else
-                    {
-                        userInput = userInput.Insert(0, "20");
-                    }
-
                 }
-
+                //remove the -+ sign so we can reuse our solution for level 1
                 userInput = userInput.Remove(8, 1);
-
-
-
-
-
             }
-
-
-
-
             if (userInput.Length != 12)
             {
                 Console.WriteLine("Felaktigt personnummer, fel antal");
@@ -76,7 +61,6 @@ namespace Personnummer
                 int day = int.Parse(userInput.Substring(6, 2));
                 int birthnumber = int.Parse(userInput.Substring(8, 3));
                 int cNumber = int.Parse(userInput.Substring(11, 1));
-
                 if (LeapYear(year))
                 {
                     //if its a leap year february is 29 days long
@@ -100,21 +84,14 @@ namespace Personnummer
                 {
                     Console.WriteLine("Felaktigt personnummer, fel kontrollsiffra " + userInput);
                 }
-
                 else
                 {
                     Console.WriteLine("Personnummer {0} är korrekt och tillhör en {1}", userInput, CheckGender(birthnumber));
                 }
-
-
-
             }
-
-
+            //Stop
             Console.ReadKey();
         }
-
-
         //returns true if the year is a leapyear
         static bool LeapYear(int year)
         {
@@ -133,7 +110,6 @@ namespace Personnummer
             //else
             return false;
         }
-
         static string CheckGender(int birth)
         {
             //Even last number is a woman, odd man
@@ -141,29 +117,23 @@ namespace Personnummer
             {
                 return "kvinna";
             }
-
             return "man";
         }
-
         static bool LuhnCheck(string formatedUserInput, int cNumber)
         {
             int sum;
             string split = "";
-
             for (int i = 1; i < formatedUserInput.Length; i++)
             {
                 //% 2 + 1 to alternate between multiplying by 1 and 2
                 sum = (int.Parse(formatedUserInput.Substring(i - 1, 1)) * ((i % 2) + 1));
                 split += (sum.ToString());
-
             }
             sum = 0;
             for (int i = 0; i < split.Length; i++)
             {
                 sum += int.Parse(split.Substring(i, 1));
             }
-
-
             //Sum is removed from nearest higher multiple of ten 
             //https://sv.wikipedia.org/wiki/Personnummer_i_Sverige#Kontrollsiffran
             if (10 - (sum % 10) % 10 == cNumber)
@@ -172,22 +142,56 @@ namespace Personnummer
             }
             return false;
         }
-
         static bool CompareToCurrentDate(string userInputDate)
         {
             DateTime date = DateTime.Now;
             int userYear = int.Parse(userInputDate.Substring(0, 2));
             int userMonth = int.Parse(userInputDate.Substring(2, 2));
             int userDay = int.Parse(userInputDate.Substring(4, 2));
-
             //returns true if current time is(without century) is higher than userinputed time
-            //this means we should subtract a century if there is a + sign instead of - in personnummer
-            if ((date.Year-2000) >= userYear && date.Month >= userMonth && date.Day > userDay)
+            //this means we should treat the person as born during the 1900s
+            if ((date.Year - 2000) > userYear)
+            {
+                return true;
+            }
+            else if ((date.Year - 2000) == userYear && (date.Month > userMonth))
+            {
+                return true;
+            }
+            else if ((date.Year - 2000) == userYear && (date.Month == userMonth) && date.Day >= userDay)
             {
                 return true;
             }
             return false;
+        }
 
+        static bool CheckIfOverHundredYears(string userInputDate)
+        {
+            DateTime date = DateTime.Now;
+            if (!CompareToCurrentDate(userInputDate))
+            {
+                userInputDate = userInputDate.Insert(0, "19");
+            }
+            else
+            {
+                userInputDate = userInputDate.Insert(0, "20");
+            }
+            int userYear = int.Parse(userInputDate.Substring(0, 4));
+            int userMonth = int.Parse(userInputDate.Substring(4, 2));
+            int userDay = int.Parse(userInputDate.Substring(6, 2));
+            if (date.Year - userYear > 0)
+            {
+                return true;
+            }
+            else if (date.Year - userYear == 0 && date.Month - userMonth > 0)
+            {
+                return true;
+            }
+            else if (date.Year - userYear == 0 && date.Month - userMonth == 0 && date.Day - userDay > 0)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
